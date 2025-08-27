9fcdca399ec568b8f4f63998b00b288495811c3b
dk <161637395+VBJM053@users.noreply.github.com>
2025-08-27 15:07:28 +0200

    Add files via upload

---
+++ dkeca_login/ViewModel/LoginViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace dkeca_login.ViewModel
+{
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+
+}
